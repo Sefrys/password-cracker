646d06d9b8cd3f3efbe1049a0044eeaa364e2408
Irek Zagan <Sefrys@gmail.com>
2018-07-10 10:14:37 +0200

    Add isPossibleRange method

--- src/main/java/model/BinarySearch.java
+++ src/main/java/model/BinarySearch.java
@@ -8,22 +8,38 @@ public class BinarySearch {
     private int INIT_FILE_NUMBER;
     private int currentFileNumber;
     private int previousFileNumber;
+    private int searchRange;
     private String password;
     private String filePath;
     private boolean possibleRangeFound;
     private boolean passwordFound;
 
-    public BinarySearch(int initialFileNumber, String password, String filePath) {
+    public BinarySearch(int initialFileNumber, String password, String filePath, int searchRange) {
         this.INIT_FILE_NUMBER = initialFileNumber;
         this.currentFileNumber = INIT_FILE_NUMBER;
+        this.searchRange = searchRange;
         this.password = password;
         this.filePath = filePath;
         this.possibleRangeFound = false;
         this.passwordFound = false;
     }
 
-    private void findePossibleFileRange() {
+    private void findPossibleFileRange() {
+        String lookedUpHash = readFirstLine(currentFileNumber);
 
+        if (isPossibleRange()) {
+            possibleRangeFound = true;
+        }
+
+    }
+
+
+    private boolean isPossibleRange() {
+        if (Math.abs(previousFileNumber - currentFileNumber) < searchRange) {
+            possibleRangeFound = true;
+            return true;
+        }
+        return false;
     }
 
     private String readFirstLine(int fileNumber) {
@@ -31,7 +47,7 @@ public class BinarySearch {
         try {
             BufferedReader br = new BufferedReader(new FileReader(filePath + "Passwords_" + fileNumber + ".txt"));
             line = br.readLine();
-            
+
             if (line == null) {
                 throw new NullPointerException("First line in file nr: " + fileNumber + " is empty");
             }
