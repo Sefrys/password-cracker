21ea437feec6be1d96ea8a2c58dd584910c3b8e8
Irek Zagan <Sefrys@gmail.com>
2018-07-10 11:10:50 +0200

    Implement binarySearch

--- src/main/java/model/BinarySearch.java
+++ src/main/java/model/BinarySearch.java
@@ -10,19 +10,30 @@ public class BinarySearch {
     private int currentFileNumber;
     private int previousFileNumber;
     private int searchRange;
+    private String breachCount;
     private String password;
     private String filePath;
     private boolean possibleRangeFound;
     private boolean passwordFound;
 
-    public BinarySearch(int initialFileNumber, String password, String filePath, int searchRange) {
-        this.INIT_FILE_NUMBER = initialFileNumber;
+    public BinarySearch(int fileCount, String password, String filePath, int searchRange) {
+        this.INIT_FILE_NUMBER = fileCount / 2;
         this.currentFileNumber = INIT_FILE_NUMBER;
         this.searchRange = searchRange;
         this.password = password;
         this.filePath = filePath;
         this.possibleRangeFound = false;
         this.passwordFound = false;
+    }
+
+    public void binarySearch() {
+        while (!possibleRangeFound) {
+            findPossibleFileRange();
+        }
+
+        while (!passwordFound) {
+            readFile();
+        }
     }
 
     private void findPossibleFileRange() {
@@ -92,8 +103,50 @@ public class BinarySearch {
         return parseLineGetHex(line);
     }
 
+    private void readFile() {
+        int lowerBound = currentFileNumber - searchRange;
+        int upperBound = currentFileNumber + searchRange;
+
+        for (int i = lowerBound; i <= upperBound; i++) {
+            System.out.println("Looking in file nr: " + i);
+            if (readLines(i)) {
+                System.out.println("Password found. Breached times: " + breachCount);
+                break;
+            }
+        }
+    }
+
+    private boolean readLines(int fileNumber) {
+        String line;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath + "Passwords_" + fileNumber + ".txt"));
+            while ((line = br.readLine()) != null) {
+                if (compareHexValues(parseLineGetHex(line)) == 0) {
+                    this.breachCount = parseLineGetBreachCount(line);
+                    this.passwordFound = true;
+                    return true;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    private int validateFileNumberBound(int fileNumberBound) {
+        if (fileNumberBound < 1) return 1;
+        if (fileNumberBound > 1004) return 1004;
+        return fileNumberBound;
+    }
+
     private String parseLineGetHex(String line) {
         return line.substring(0, line.indexOf(':'));
+    }
+
+    private String parseLineGetBreachCount(String line) {
+        return line.substring(line.indexOf(':') + 1);
     }
 
 }
