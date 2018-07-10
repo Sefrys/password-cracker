24b6c8e18f6d994db9d31de081f59cc838ede496
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-10 13:14:26 +0200

    Add InputGetter class

---
+++ src/main/java/service/InputGetter.java
@@ -0,0 +1,24 @@
+package main.java.service;
+
+import java.util.Scanner;
+
+public class InputGetter {
+
+    private Scanner scanner;
+
+    public String takeUserInput() {
+        String userInput;
+        initializeScanner();
+
+        while(scanner.hasNext()) {
+            userInput = scanner.nextLine();
+
+            if (userInput != null) return userInput;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void initializeScanner() {
+        scanner = new Scanner(System.in);
+    }
+}
