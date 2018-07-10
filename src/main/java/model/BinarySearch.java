f7bf66ab63bc89f42296ccd56110472c39894c80
Irek Zagan <Sefrys@gmail.com>
2018-07-10 09:45:04 +0200

    Add filePath field

--- src/main/java/model/BinarySearch.java
+++ src/main/java/model/BinarySearch.java
@@ -1,18 +1,24 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class BinarySearch {
     private int INIT_FILE_NUMBER;
     private int currentFileNumber;
     private int previousFileNumber;
     private String password;
+    private String filePath;
     private boolean possibleRangeFound;
     private boolean passwordFound;
 
-    public BinarySearch(int initialFileNumber, String password) {
+    public BinarySearch(int initialFileNumber, String password, String filePath) {
         this.INIT_FILE_NUMBER = initialFileNumber;
         this.currentFileNumber = INIT_FILE_NUMBER;
         this.password = password;
+        this.filePath = filePath;
         this.possibleRangeFound = false;
         this.passwordFound = false;
     }
+
 }
