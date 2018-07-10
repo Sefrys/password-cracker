37f105b58d35130a59bda13e9001b3b18f830989
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-10 13:47:50 +0200

    Add getters to Password class

--- src/main/java/model/Password.java
+++ src/main/java/model/Password.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Password {
+
+    private String password;
+    private int length;
+    private char[] array;
+//    private int level;
+
+    public Password(String password) {
+        this.password = password;
+        this.length = password.length();
+        this.array = password.toCharArray();
+//        this.level = 0;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public char[] getArray() {
+        return array;
+    }
+
+//    public int getLevel() {
+//        return level;
+//    }
 }
