4fd7ed3f6d4caebea886bee2193b83dc6885f4c2
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-10 13:48:52 +0200

    Add working while loop for menu

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,13 +1,32 @@
 package main.java.controller;
 
-public class MainController {
+import main.java.service.InputGetter;
+import main.java.view.View;
 
-    private static final String exit = "exit";
+
+public class MainController {
+    private InputGetter inputGetter;
+    private View view;
+
+    private static final String checkPassword = "1";
+    private static final String exit = "2";
+
 
     private boolean isAppEnd = false;
 
+    public MainController() {
+        this.inputGetter = new InputGetter();
+        this.view = new View();
+    }
+
     public void runApp() {
         while (!isAppEnd) {
+            view.displayMenu();
+            String userInput = inputGetter.takeUserInput();
+
+            if (checkPassword.equals(userInput)) {
+                ;
+            }
 
             if (exit.equals(userInput)) {
                 isAppEnd = true;
