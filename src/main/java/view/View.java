ab210a4b672d9e61c577089fff3bc0fb97841147
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-10 13:32:48 +0200

    Add View class, displayMenu method

---
+++ src/main/java/view/View.java
@@ -0,0 +1,19 @@
+package main.java.view;
+
+public class View {
+
+    private static final String passwordCracker = "Password cracker";
+    private static final String mainMenuLabel = "Main menu";
+    private static final String[] menuOptions =
+            new String[] {"1. Enter password", "2. Exit"};
+
+    public void displayMenu() {
+        System.out.println(passwordCracker + "\n");
+        System.out.println(mainMenuLabel);
+
+        for (String menuOption : menuOptions) {
+            System.out.println(menuOption);
+        }
+    }
+
+}
