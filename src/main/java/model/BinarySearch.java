8377a32414df8f8d12387ea2b284d42bf56f8eba
Irek Zagan <Sefrys@gmail.com>
2018-07-10 08:56:21 +0200

    Declare fields

---
+++ src/main/java/model/BinarySearch.java
@@ -0,0 +1,11 @@
+package model;
+
+public class BinarySearch {
+    private int INIT_FILE_NUMBER;
+    private int currentFileNumber;
+    private int previousFileNumber;
+    private String password;
+    private boolean possibleRangeFound;
+    private boolean passwordFound;
+
+}
