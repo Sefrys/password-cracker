91f4d34ec0daaec389bbd0c9b3a0738222ff84a3
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-11 10:54:06 +0200

    Add method validate and private methods to PasswordService class

--- src/main/java/service/PasswordService.java
+++ src/main/java/service/PasswordService.java
@@ -1,4 +1,31 @@
 package service;
 
 public class PasswordService {
+
+    public static Enum validate(String password) {
+        boolean containsDigits = false;
+        boolean containsCharacter = false;
+
+        for (char letter : password.toCharArray()) {
+            if (isLetter(letter)) {
+                containsCharacter = true;
+            } else if (isDigit(letter)) {
+                containsDigits = true;
+            }
+        }
+
+        if (containsCharacter && containsDigits) return Complexity.ADVANCED;
+        else if (containsDigits && !containsCharacter) return Complexity.EASY;
+        else if (!containsDigits && containsCharacter) return Complexity.MEDIUM;
+        return Complexity.ERROR;
+    }
+
+    private static boolean isLetter(char c) {
+        return (c >= 'A' && c <= 'Z');
+    }
+
+    private static boolean isDigit(char c) {
+       return (c >= '0' && c <= '9');
+    }
+
 }
