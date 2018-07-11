68d7c62971f3c9d44bd7b124283ecfb2bcf0783e
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-11 13:43:35 +0200

    Prepare app to run for testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import controller.BinarySearchController;
+import controller.MainController;
 import model.BruteForcer;
 import model.Comparator;
 import model.Password;
@@ -8,26 +9,28 @@ import java.io.FileNotFoundException;
 
 public class App {
     public static void main(String[] args) {
+//
+//        try {
+//            BinarySearchController bsController = new BinarySearchController(1004, 10, "password", "Passwords/");
+//            BinarySearchView bsView = new BinarySearchView();
+//            bsController.binarySearch();
+//            if (bsController.getBinarySearch().isSearchFinished()) {
+//                bsView.printLine("Search finished.");
+//            }
+//            if (bsController.getBinarySearch().isPasswordFound()) {
+//                bsView.printLine("Password found. Times breached: " + bsController.getBinarySearch().getBreachCount());
+//            } else {
+//                bsView.printLine("Password is not in databse");
+//            }
+//        }catch (Throwable e ){
+//            //e.printStackTrace();
+//        }
+//
+//        String passwd = "0AA";
+//        new Comparator(passwd);
+//        (new Thread(new BruteForcer(new Password(passwd), (char) 48 ))).start();
 
-        try {
-            BinarySearchController bsController = new BinarySearchController(1004, 10, "password", "Passwords/");
-            BinarySearchView bsView = new BinarySearchView();
-            bsController.binarySearch();
-            if (bsController.getBinarySearch().isSearchFinished()) {
-                bsView.printLine("Search finished.");
-            }
-            if (bsController.getBinarySearch().isPasswordFound()) {
-                bsView.printLine("Password found. Times breached: " + bsController.getBinarySearch().getBreachCount());
-            } else {
-                bsView.printLine("Password is not in databse");
-            }
-        }catch (Throwable e ){
-            //e.printStackTrace();
-        }
-
-        String passwd = "0AA";
-        new Comparator(passwd);
-        (new Thread(new BruteForcer(new Password(passwd), (char) 48 ))).start();
-
+        MainController mainController = new MainController();
+        mainController.runApp();
     }
 }
