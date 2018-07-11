1303f385ca2cce7ce6542280b503fbe3bbeea1b2
drmayx <mj6367@gmail.com>
2018-07-11 11:03:53 +0200

    Update bruteforcer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,23 @@
 import model.BinarySearch;
+import model.BruteForcer;
+import model.Password;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
     public static void main(String[] args) {
-        int fileCount = 1004;
-        String password = sha1("1qazxsw2");
-        String filePath = "Passwords/";
-        int searchRange = 10;
-        BinarySearch bs = new BinarySearch(fileCount, password, filePath, searchRange);
-        bs.binarySearch();
+
+
+
+//        int fileCount = 1004;
+//        String password = sha1("1qazxsw2");
+//        String filePath = "Passwords/";
+//        int searchRange = 10;
+//        BinarySearch bs = new BinarySearch(fileCount, password, filePath, searchRange);
+//        bs.binarySearch();
+
+        (new Thread(new BruteForcer(new Password("0zzz"), (char) 48 ))).start();
     }
 
     public static String sha1(String input) {
--- src/main/java/model/BruteForcer.java
+++ src/main/java/model/BruteForcer.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Date;
+
 /**
  * Created by Maciej Jankowicz on 10.07.18, 13:17
  * Contact: mj6367@gmail.com
@@ -8,27 +10,31 @@ public class BruteForcer implements Runnable{
 
     private char firstChar;
     private int length;
-    private char[] possibleValues;
+    private int possibleValues;
     private char[] startingSequence;
-    private boolean found;
     private String password;
 
 
-    public BruteForcer(Password password){
-        this.password = password.getWord();
-        this.firstChar = password.getCharArray()[0];
-        this.length = password.getLenght();
-        this.possibleValues = password.getComplexity();
+    public BruteForcer(Password password, char firstChar){
+        this.password = password.getPassword();
+        this.length = password.getLength();
+        this.firstChar = firstChar;
+        this.possibleValues = 10 ;//password.getComplexity().getComplexity();
         this.startingSequence = new char[this.length];
         this.startingSequence[0] = this.firstChar;
         for(int i = 1; i < this.length; i++){
-            this.startingSequence[i] = this.possibleValues[0];
+            this.startingSequence[i] = '0';
         }
     }
 
     @Override
     public void run() {
-        found = search();
+        System.out.println("running");
+        Date start = new Date();
+        boolean found = search();
+        Date end = new Date();
+        if(found) System.out.println("it took: " + (end.getTime() - start.getTime()) + "ms");
+        System.out.println("end");
     }
 
     private boolean search(){
@@ -36,12 +42,24 @@ public class BruteForcer implements Runnable{
             return true;
         }
 
-        for(int i = 0; i<Math.pow(possibleValues.length, this.length); i++){
+        double charCode = -1d;
+
+        for(int i = 0; i<Math.pow(possibleValues, this.length); i++){
             for(int j = 1; j < startingSequence.length; j++){
-                startingSequence[j] = (char) ((new Double(i/(Math.pow(10,j)))).intValue()%10 + 48);
+
+                charCode = ((new Double(i/(Math.pow(possibleValues,j)))).intValue()%possibleValues); //+48
+                if(charCode <10) {                      //numbers
+                    charCode += 48;
+                }else if(charCode>=10 && charCode<36){  //letters
+                    charCode += 55;
+                }else if(charCode >=36 && charCode<62){
+                    charCode += 61;
+                }
+                startingSequence[j] = (char) charCode;
             }
-            System.out.println(String.valueOf(startingSequence));
-            if(String.valueOf(startingSequence).equals(password)){
+
+            if(Checker. (startingSequence)){
+                System.out.println("found password: " + String.valueOf(startingSequence));
                 return true;
             }
         }
