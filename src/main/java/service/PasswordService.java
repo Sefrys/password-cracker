04ac134e04d78629d10c73f0258e8c919ecf66c5
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-11 13:16:08 +0200

    Modify PasswordService class

--- src/main/java/service/PasswordService.java
+++ src/main/java/service/PasswordService.java
@@ -2,30 +2,46 @@ package service;
 
 public class PasswordService {
 
-    public static Enum validate(String password) {
-        boolean containsDigits = false;
-        boolean containsCharacter = false;
+    private static boolean containsDigits = false;
+    private static boolean containsUpperCaseCharacter = false;
+    private static boolean containsLowerCaseCharacter = false;
+    private static Complexity complexity = null;
 
-        for (char letter : password.toCharArray()) {
-            if (isLetter(letter)) {
-                containsCharacter = true;
-            } else if (isDigit(letter)) {
-                containsDigits = true;
-            }
-        }
-
-        if (containsCharacter && containsDigits) return Complexity.ADVANCED;
-        else if (containsDigits && !containsCharacter) return Complexity.EASY;
-        else if (!containsDigits && containsCharacter) return Complexity.MEDIUM;
-        return Complexity.ERROR;
+    public static void validate(String password) throws IllegalArgumentException {
+        iterate(password);
+        if (containsDigits && containsLowerCaseCharacter && containsUpperCaseCharacter) complexity = Complexity.HARD;
+        else if (containsUpperCaseCharacter && containsDigits) complexity = Complexity.ADVANCED;
+        else if (!containsDigits && containsUpperCaseCharacter) complexity = Complexity.MEDIUM;
+        else if (containsDigits && !containsUpperCaseCharacter && !containsLowerCaseCharacter) complexity = Complexity.EASY;
     }
 
-    private static boolean isLetter(char c) {
+    private static void iterate(String password) throws IllegalArgumentException {
+        for (char letter : password.toCharArray()) {
+            if (isUpperCaseLetter(letter)) {
+                containsUpperCaseCharacter = true;
+            } else if (isLowerCaseLetter(letter)) {
+                containsLowerCaseCharacter = true;
+            } else if (isDigit(letter)) {
+                containsDigits = true;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static Complexity getComplexity() {
+        return complexity;
+    }
+
+    private static boolean isUpperCaseLetter(char c) {
         return (c >= 'A' && c <= 'Z');
     }
 
     private static boolean isDigit(char c) {
-       return (c >= '0' && c <= '9');
+        return (c >= '0' && c <= '9');
     }
 
+    private static boolean isLowerCaseLetter(char c) {
+        return (c >= 'a' && c <= 'z');
+    }
 }
