53f88468eb61b58045fb0447942e7b2e60f81e3c
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-07-12 14:17:22 +0200

    Add remove method for finished threads in container

--- src/main/java/model/ThreadContainer.java
+++ src/main/java/model/ThreadContainer.java
@@ -1,8 +1,11 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+
+import static java.lang.Thread.sleep;
 
 public class ThreadContainer implements Runnable {
 
@@ -25,15 +28,36 @@ public class ThreadContainer implements Runnable {
         return result;
     }
 
+    public static void remove() {
+        Iterator<BruteForcer> iterator = threadList.iterator();
+
+        while(iterator.hasNext()) {
+            BruteForcer element = iterator.next();
+
+            if (element.isFound()) {
+                iterator.remove();
+            }
+            else {
+                iterator.remove();
+            }
+        }
+    }
+
     public void run() {
         while(true) {
             if (Thread.interrupted()) {
                 break;
             }
+            remove();
             pickRandomElement();
+
+            try {
+                sleep(2000);
+            }
+            catch (InterruptedException e) {}
         }
     }
-    
+
 
 
     private void pickRandomElement() {
@@ -46,5 +70,9 @@ public class ThreadContainer implements Runnable {
         int range = threadList.size() - 1;
 
         return random.nextInt(range);
+    }
+
+    public BruteForcer getRandomThread() {
+        return randomThread;
     }
 }
